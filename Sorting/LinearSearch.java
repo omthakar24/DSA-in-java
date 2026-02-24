cce344afbec3c9ad3c5ff54120ccd51983c2a070
omthakar24 <omthakar2411@gmail.com>
2026-02-25 00:12:24 +0530

    LinearSearch added

---
+++ Sorting/LinearSearch.java
@@ -0,0 +1,18 @@
+package ArrayProblems;
+
+public class LinearSearch {
+    static int Linear(int[]arr, int n){
+
+          for(int i = 0; i<arr.length;i++){
+            if(arr[i]==n){
+               return i;
+            }
+    }return-1;
+}
+    public static void main(String []args){
+        int[] arr = {5,4,3,2,1,5,4,3,2,1};
+        int n = 5;
+        int result = Linear(arr,n);
+        System.out.println(result);
+        }
+}
